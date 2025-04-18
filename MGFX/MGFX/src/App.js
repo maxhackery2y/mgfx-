ff53ddf1d45b2af987c0167922411bc364f29c0f
maxhackery2y <157688940+maxhackery2y@users.noreply.github.com>
2025-04-18 15:10:33 +0600

    Add files via upload

---
+++ MGFX/MGFX/src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ color: 'white', backgroundColor: '#111', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Welcome to MGFX</h1>
+    </div>
+  );
+}
+
+export default App;
